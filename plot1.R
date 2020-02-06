f4c36e69247b51ccdd42535434258d0c223b59d4
Andrea Giugno <andrea.giugno86@gmail.com>
2020-02-06 21:31:59 +0100

    Source code optimized

--- plot1.R
+++ plot1.R
@@ -10,10 +10,8 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 # Compute totals
-Totals <- aggregate(Emissions ~ year, NEI, sum)
-totalPM25 <- as.data.frame(Totals$Emissions, 
-                           row.names = as.character(Totals$year))
-names(totalPM25) <- "TotalEmissions"
+totalPM25 <- aggregate(Emissions ~ year, NEI, sum)
+names(totalPM25) <- c("year", "TotalEmissions")
 
 # Make the plot
 png("plot1.png", height = 480, width = 480, units = "px")
@@ -21,7 +19,7 @@ barplot(totalPM25$TotalEmissions,
         main = "Total amount of emissions in the observed years",
         xlab = "Years", ylab = "Total PM25 emissions",
         col = "red",
-        names.arg = row.names(totalPM25))
+        names.arg = row.names(totalPM25$year))
 dev.off()
 
 #Yes, the total amount of emissions has steadily decreased.
